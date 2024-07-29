d462821a62565ac9ef05904b2150d42be221eec7
Murphy <mofei@starrocks.com>
2024-07-29 20:24:12 +0800

    optimize massive or predicates
    Signed-off-by: Murphy <mofei@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/ReplaceSubqueryRewriteRule.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/rewrite/scalar/ReplaceSubqueryRewriteRule.java
@@ -15,6 +15,7 @@
 
 package com.starrocks.sql.optimizer.rewrite.scalar;
 
+import com.starrocks.sql.optimizer.Utils;
 import com.starrocks.sql.optimizer.operator.logical.LogicalApplyOperator;
 import com.starrocks.sql.optimizer.operator.scalar.ScalarOperator;
 import com.starrocks.sql.optimizer.operator.scalar.SubqueryOperator;
@@ -49,11 +50,14 @@ public class ReplaceSubqueryRewriteRule extends TopDownScalarOperatorRewriteRule
 
     @Override
     public ScalarOperator visit(ScalarOperator scalarOperator, ScalarOperatorRewriteContext context) {
-        if (subqueryPlaceholders == null) {
-            return scalarOperator;
+        if (subqueryPlaceholders == null || subqueryPlaceholders.isEmpty()) {
+                return scalarOperator;
         }
-        if (subqueryPlaceholders.containsKey(scalarOperator)) {
-            SubqueryOperator subqueryOperator = subqueryPlaceholders.get(scalarOperator);
+        // Usually subqueryPlaceholders's key set is small and only contain columnRef operator
+        // so columnRef operator's equals()' short-circuit can benefit complex ScalarOperator like one thousand or predicate
+        // if use Map::containsKey, these complex ScalarOperator's hashCode can be super slow
+        SubqueryOperator subqueryOperator = Utils.getValueIfExists(subqueryPlaceholders, scalarOperator);
+        if (subqueryOperator != null) {
             LogicalApplyOperator applyOperator = subqueryOperator.getApplyOperator();
             builder = new OptExprBuilder(applyOperator, Arrays.asList(builder, subqueryOperator.getRootBuilder()),
                     builder.getExpressionMapping());
